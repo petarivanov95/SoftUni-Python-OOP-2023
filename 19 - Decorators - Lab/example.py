7842c89594c7588c309fd2843f10d513daa911b3
Petar Ivanov <petarivanov95@gmail.com>
2023-03-22 22:07:26 +0200

    Update example.py

--- 19 - Decorators - Lab/example.py
+++ 19 - Decorators - Lab/example.py
@@ -1,14 +1,40 @@
-def my_decorator(func):
-    def wrapper():
-        print("Before the function is called")
-        func()
-        print("After the function is called")
+# def my_decorator(func):
+#     def wrapper():
+#         print("Before the function is called")
+#         func()
+#         print("After the function is called")
+#
+#     return wrapper
+#
+#
+# @my_decorator
+# def hello():
+#     print("Hello")
+#
+# hello()
+#
+#
+# def outer():
+#     a = 25
+#     name = 'Python'
+#
+#     def inner(prefix):
+#         print(prefix, name)
+#
+#     return inner
 
-    return wrapper
+def layer_1():
+    def layer_2(num2):
+        print(num2 * 'Layer 2 ')
+        def layer_3(num3):
+            print(num3 * 'Layer 3 ')
+        return layer_3
+    return layer_2
 
 
-@my_decorator
-def hello():
-    print("Hello")
+b = layer_1()
+c = b(3)
+d = c(4)
 
-hello()
+
+
