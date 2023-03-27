90c36eea7bed2d3d6fb4a96a34e78433ead15a7c
Petar Ivanov <petarivanov95@gmail.com>
2023-03-27 22:14:37 +0300

    Update food_orders_app.py

--- 00 - Exam Prep/python-oop-exam-2022-08-22/01. Structure_Skeleton/project/food_orders_app.py
+++ 00 - Exam Prep/python-oop-exam-2022-08-22/01. Structure_Skeleton/project/food_orders_app.py
@@ -1,10 +1,27 @@
 from project.meals.starter import Starter
 from project.meals.dessert import Dessert
 from project.meals.main_dish import MainDish
-import client
+from project.client import Client
 
 
-class FoodOrdersApp():
-    
+class FoodOrdersApp:
+    VALID_MEALS = {'Starter': Starter, 'Dessert': Dessert,'Main Dish': MainDish}
+
+    def __init__(self):
+        self.menu = []
+        self.clients_list = []
+
+    def register_client(self, client_phone_number:str):
+        for client in self.clients_list:
+            if client_phone_number == client.phone_number:
+                raise Exception("The client has already been registered!")
+
+        client = Client(client_phone_number)
+        self.clients_list.append(client)
+
+    def 
+
+
+
 
 food_orders_app = FoodOrdersApp()
