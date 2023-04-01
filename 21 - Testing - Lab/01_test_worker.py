f7b716fae22ddf649a3284504ac67ec0ec26d087
Petar Ivanov <petarivanov95@gmail.com>
2023-04-01 11:57:26 +0300

    01_test_worker update

--- 21 - Testing - Lab/01_test_worker.py
+++ 21 - Testing - Lab/01_test_worker.py
@@ -34,15 +34,29 @@ class WorkerTests(unittest.TestCase):
         self.assertEqual(self.worker.money, 0)
 
     def test_energy_increment_with_rest(self):
-        worker = Worker('John', 1000, 200)
-        worker.rest()
-        self.assertEqual(worker.energy, 201)
+        self.worker.rest()
+        self.assertEqual(self.worker.energy, 101)
 
     def test_work_with_negative_energy(self):
         with self.assertRaises(Exception) as e:
             worker = Worker('John', 1000, -1)
             worker.work()
         self.assertEqual(str(e.exception), 'Not enough energy.')
+
+    def test_salary_increase_with_work_method(self):
+        self.worker.work()
+        self.assertEqual(self.worker.money, 1000)
+
+    def test_energy_decrease_with_work_method(self):
+        self.worker.work()
+        self.assertEqual(self.worker.energy, 99)
+
+    def test_get_info_method(self):
+        result = self.worker.get_info()
+        expected_result = 'John has saved 0 money.'
+        self.assertEqual(result,expected_result)
+
+
 
 if __name__ == '__main__':
     unittest.main()
