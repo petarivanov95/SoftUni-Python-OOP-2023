95ce4abeb4f108a9d211ec5ccd018bd1d92e5855
Petar Ivanov <petarivanov95@gmail.com>
2023-04-04 20:47:51 +0300

    added edit_movie method and check_if_movie_exists method

--- 00 - Exam Prep/python-oop-exam-2022-04-18/01. Structure_Skeleton/project/movie_app.py
+++ 00 - Exam Prep/python-oop-exam-2022-04-18/01. Structure_Skeleton/project/movie_app.py
@@ -9,6 +9,13 @@ class MovieApp:
         self.movies_collection = []
         self.users_collection = []
 
+    def check_if_movie_exists(self, title):
+        for movie in self.movies_collection:
+            if movie.title == title:
+                return True
+        return False
+
+
     def register_user(self, username: str, age: int):
         for user in self.users_collection:
             if user.username == username:
@@ -32,7 +39,15 @@ class MovieApp:
         self.movies_collection.append(movie)
         return f"{username} successfully added {movie.title} movie."
 
-    
+    def edit_movie(self, username, movie, **kwargs):
+        if not self.check_if_movie_exists(movie.title):
+            raise Exception(f'The movie {movie.title} is not uploaded!')
+        elif not username == movie.owner.username:
+            raise Exception(f'{username} is not the owner of the movie {movie.title}!')
+        else:
+            for attr, new_value in kwargs.items():
+                setattr(movie, attr, new_value)
+            return f'{username} successfully edited {movie.title} movie.'
 
 
 movie_app = MovieApp()
@@ -45,7 +60,7 @@ print(movie_app.register_user('Alexandra', 25))
 user2 = movie_app.users_collection[1]
 movie2 = Action('Free Guy', 2021, user2, 16)
 print(movie_app.upload_movie('Alexandra', movie2))
-# print(movie_app.edit_movie('Alexandra', movie2, title="Free Guy 2"))
+print(movie_app.edit_movie('Alexandra', movie2, title="Free Guy 2"))
 # print(movie_app.like_movie('Martin', movie2))
 # print(movie_app.like_movie('Alexandra', movie))
 # print(movie_app.dislike_movie('Martin', movie2))
